27e0230fd69b8a66da17a0593edb0f95cd29a5fa
James Chuong <chuong.james@gmail.com>
2023-04-10 01:31:31 -0700

    Add picocss to client

---
+++ client/src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import '@picocss/pico';
+</script>
+
+<header class="container-fluid">
+    <nav>
+        <ul>
+            <li>Hako</li>
+        </ul>
+        <ul>
+            <li>
+                Some link
+            </li>
+        </ul>
+    </nav>
+</header>
+<main class="container-fluid">
+    <slot />
+</main>
