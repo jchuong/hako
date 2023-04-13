e61d4efb517004afe1c671c80d6711f6319d5672
James Chuong <chuong.james@gmail.com>
2023-04-12 17:21:44 -0700

    Initial persistent theme toggle

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,11 +1,20 @@
 <script lang="ts">
 	import '@picocss/pico';
+    import theme from '$lib/shared/stores/theme';
+	import { browser } from '$app/environment';
 
 	let isDarkMode = false;
+    theme.subscribe(t => {
+        isDarkMode = t === 'dark';
+    })
     function handleTheme() {
         const newTheme = isDarkMode ? 'light' : 'dark';
+        theme.set(newTheme);
         document.getElementById('root')?.setAttribute('data-theme', newTheme);
     };
+	if (browser) {
+		document.getElementById('root')?.setAttribute('data-theme', $theme);
+	}
 </script>
 
 <header class="container-fluid">
