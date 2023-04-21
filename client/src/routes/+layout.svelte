46addcb2c7cd582fe7da35d737281b2c3900db28
James Chuong <chuong.james@gmail.com>
2023-04-20 23:16:24 -0700

    Add version to layout footer
    added layout to fill viewport

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,23 +1,41 @@
 <script lang="ts">
 	import '@picocss/pico';
+	import { version } from '$app/environment';
 	import ThemeSwitcher from '$lib/components/ThemeSwitcher.svelte';
 </script>
 
-<header class="container-fluid">
-	<nav>
-		<ul>
-			<li>Hako</li>
-		</ul>
-		<ul>
-			<li>Some link</li>
-		</ul>
-		<ul>
-			<li>
-				<ThemeSwitcher />
-			</li>
-		</ul>
-	</nav>
-</header>
-<main class="container-fluid">
-	<slot />
-</main>
+<div class="layout">
+	<header class="container-fluid">
+		<nav>
+			<ul>
+				<li>Hako</li>
+			</ul>
+			<ul>
+				<li>Some link</li>
+			</ul>
+			<ul>
+				<li>
+					<ThemeSwitcher />
+				</li>
+			</ul>
+		</nav>
+	</header>
+	<main class="container-fluid main">
+		<slot />
+	</main>
+	<footer class="container-fluid"><small>{version}</small></footer>
+</div>
+
+<style>
+	.layout {
+		min-height: 100dvh;
+		position: relative;
+		overflow: auto;
+	}
+	footer {
+		position: absolute;
+		bottom: 0;
+		display: flex;
+		justify-content: center;
+	}
+</style>
