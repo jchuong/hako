90947ed36383de0d2ff8f7c9fb7e45b6ee13e9fb
James Chuong <chuong.james@gmail.com>
2023-04-21 00:28:38 -0700

    Initial commit for tag models
    Added tags app to handle tags

---
+++ server/hako/apps/tags/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Tag(models.Model):
+    name = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+    tags = models.ManyToManyField(Tag)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
