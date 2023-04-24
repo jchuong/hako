42f388aff331793b0bbcbaf10d29ca73b7b51f4e
James Chuong <chuong.james@gmail.com>
2023-04-23 18:27:01 -0700

    Initial CRUD for tags + migrations

--- server/hako/apps/tags/models.py
+++ server/hako/apps/tags/models.py
@@ -1,12 +1,23 @@
 from django.db import models
 
-class Tag(models.Model):
-    name = models.CharField(max_length=128)
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-class Category(models.Model):
-    name = models.CharField(max_length=128)
-    tags = models.ManyToManyField(Tag)
+    class Meta:
+        indexes = [
+            models.Index(fields=['name'])
+        ]
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    category = models.ForeignKey(Category, default=None, on_delete=models.SET_NULL, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['name'])
+        ]
