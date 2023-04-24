1f2575b54311295b75114b49d390f7743bdb257f
James Chuong <chuong.james@gmail.com>
2023-04-23 19:17:10 -0700

    Add ruff and black
    Added linter and formatter, and fixed found issues with black

--- server/hako/apps/tags/models.py
+++ server/hako/apps/tags/models.py
@@ -1,23 +1,22 @@
 from django.db import models
 
+
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        indexes = [
-            models.Index(fields=['name'])
-        ]
+        indexes = [models.Index(fields=["name"])]
 
 
 class Tag(models.Model):
     name = models.CharField(max_length=128, unique=True)
-    category = models.ForeignKey(Category, default=None, on_delete=models.SET_NULL, null=True, blank=True)
+    category = models.ForeignKey(
+        Category, default=None, on_delete=models.SET_NULL, null=True, blank=True
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        indexes = [
-            models.Index(fields=['name'])
-        ]
+        indexes = [models.Index(fields=["name"])]
