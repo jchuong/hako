47dbed9b1667f2eedba115c678685a4436018e36
James Chuong <chuong.james@gmail.com>
2023-06-18 23:23:24 -0700

    Format and add prettierignore

--- client/src/lib/api/api.ts
+++ client/src/lib/api/api.ts
@@ -1,20 +1,17 @@
-import { Apity } from '@cocreators-ee/apity'
+import { Apity } from '@cocreators-ee/apity';
 import type { paths } from '$lib/api/types';
 
-const apity = Apity.for<paths>()
+const apity = Apity.for<paths>();
 
 // global configuration
 apity.configure({
-  // Base URL to your API
-  baseUrl: 'http://localhost:8000',
-  // RequestInit options, e.g. default headers
-  init: {
-    mode: 'same-origin',
-  },
+	// Base URL to your API
+	baseUrl: 'http://localhost:8000',
+	// RequestInit options, e.g. default headers
+	init: {
+		mode: 'same-origin'
+	}
 });
 
 // create fetch operations
-export const getTags = apity
-  .path('/api/tags')
-  .method('get')
-  .create();
+export const getTags = apity.path('/api/tags').method('get').create();
